0f638278caec3e8c1ecd5944805e6288fc821c2a
DACHANCHOI <dachan1207@gmail.com>
2022-04-15 15:36:22 +0900

    remote 주석

--- _remote.tf
+++ _remote.tf
@@ -1,8 +1,8 @@
-data "terraform_remote_state" "vpc" {
-  backend = "s3"
-  config = {
-    bucket = var.backend_s3
-    key    = var.vpc_key
-    region = var.region
-  }
-}
+// data "terraform_remote_state" "vpc" {
+//   backend = "s3"
+//   config = {
+//     bucket = var.backend_s3
+//     key    = var.vpc_key
+//     region = var.region
+//   }
+// }
